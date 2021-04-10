53807a77583e9bf92c8a36bbc0d953b5aa0ee079
Suthinan Musitmani <suthinan.musitmani@gmail.com>
2021-04-10 19:21:13 +0700

    init

--- index.js
+++ index.js
@@ -1,32 +1,6 @@
 const core = require('@actions/core');
 const axios = require('axios');
 
-const config = {
-    url: '/user',
-    method: 'get',
-    baseURL: 'https://some-domain.com/api/',
-    headers: { 'X-Requested-With': 'XMLHttpRequest' },
-    params: {
-        ID: 12345
-    },
-    data: {
-        firstName: 'Fred'
-    },
-    timeout: 30000,
-    auth: {
-        username: 'janedoe',
-        password: 's00pers3cret'
-    },
-    proxy: {
-        protocol: 'https',
-        host: '127.0.0.1',
-        port: 9000,
-        auth: {
-            username: 'mikeymike',
-            password: 'rapunz3l'
-        }
-    }
-};
 try {
     const inputKeyArr = ['url', 'method', 'baseURL', 'headers', 'params', 'data', 'timeout', 'auth', 'proxy'];
     const config = {};
