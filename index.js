afbfdd2f588c5955ebd81693ca54038e354c3d11
Suthinan Musitmani <suthinan.musitmani@gmail.com>
2021-04-10 20:54:30 +0700

    test cron

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ try {
         if (!!input) {
             if (input[0] === '{') {
                 config[inputKey] = JSON.parse(input);
+                console.log(config[inputKey]);
             } else {
                 config[inputKey] = input;
             }
